7a11bf438ce3c2ce380c90c4dad517eaaa5af143
peso4eek <grizimindmit@mail.ru>
2024-12-18 19:43:29 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        
         Host botik = new Host("7914453101:AAGSn3sk1LGEPiTG5Q1gvgbEEa17F1E6Hqo");
         botik.Start();
         Console.ReadLine();
